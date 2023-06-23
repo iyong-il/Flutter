583c009425c63b1753958eb7e408a652a42260ae
iyong-il <lyi1233313@gmail.com>
2023-06-24 02:12:00 +0900

    리스트뷰 연습

--- my_appbar_tutorial/lib/main.dart
+++ my_appbar_tutorial/lib/main.dart
@@ -50,14 +50,54 @@ class MyAppBar extends StatelessWidget {
                 Icons.media_bluetooth_on_sharp,
                 size: 60.0,
               ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                ),
+              ],
               onDetailsPressed: () {
                 print('button Pressed4');
               },
               decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0))),
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('HOME'),
+              onTap: () {
+                print('home button tapped');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('SETTINGS'),
+              onTap: () {
+                print('settings button tapped');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A button tapped');
+              },
+              trailing: Icon(Icons.add),
             )
           ],
         ),
