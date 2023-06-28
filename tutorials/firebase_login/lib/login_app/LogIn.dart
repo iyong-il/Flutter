9b0832c8ee8d0ade113135655da2e407845024bf
iyong-il <lyi1233313@gmail.com>
2023-06-29 02:09:01 +0900

    컴포넌트 구조화 시키기

---
+++ tutorials/firebase_login/lib/login_app/LogIn.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_login/my_button/my_button.dart';
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MyButton(text: Text('Login with Google'), onPressed: () {
+              print('google button tapped');
+            }),
+            MyButton(text: Text('Login with Facebook'), onPressed: () {
+              print('Facebook button tapped');
+            }),
+            MyButton(text: Text('Login with Email'), onPressed: () {
+              print('Email button tapped');
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ tutorials/firebase_login/lib/my_button/my_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, this.text, this.onPressed});
+
+  final Widget? text;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      height: 50.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            child: text,
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(),
+          ),
+        ],
+      ),
+    );
+  }
+}
