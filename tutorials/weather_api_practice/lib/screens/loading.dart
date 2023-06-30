e122aa2efff73fae24a894126b77449c3dffd58c
iyong-il <lyi1233313@gmail.com>
2023-07-01 08:13:26 +0900

    현재 위치 가져오기 구현

---
+++ tutorials/weather_api_practice/lib/screens/loading.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Get My Location'),
+          onPressed: () {
+            print('get my location button tapped');
+            getLocation();
+          },
+        ),
+      ),
+    );
+  }
+}
