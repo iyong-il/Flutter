760e68172c8c79d920be74fd7275ba8e7a354a63
iyong-il <lyi1233313@gmail.com>
2023-07-02 02:38:50 +0900

    목업데이터로 네트워킹 해보기

--- tutorials/weather_api_practice/lib/screens/loading.dart
+++ tutorials/weather_api_practice/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -9,11 +11,12 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
   }
 
   @override
@@ -23,11 +26,38 @@ class _LoadingState extends State<Loading> {
         child: ElevatedButton(
           child: const Text('Get My Location'),
           onPressed: () {
-            print('get my location button tapped');
-            getLocation();
+            null;
           },
         ),
       ),
     );
+  }
+}
+
+void getLocation() async {
+  try {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  } catch (e) {
+    print('There was a problem with the internet connection.');
+  }
+}
+
+void fetchData() async {
+  http.Response response = await http.get(Uri.parse(
+      'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+
+  if(response.statusCode == 200) {
+    String jsonData = response.body;
+
+    var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+    var myWind = jsonDecode(jsonData)['wind']['speed'];
+    var myId = jsonDecode(jsonData)['id'];
+
+    print('myJson : $myJson');
+    print('myWind : $myWind');
+    print('myId : $myId');
   }
 }
