ff17d786c61b1268e4e02137c58c447d6e063193
iyong-il <lyi1233313@gmail.com>
2023-07-08 20:01:08 +0900

    목데이터로 리스트뷰 만들어보기

---
+++ tutorials/recipes/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// import 'recipe_model.dart';
+import 'package:recipes/recipe_model.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return buildRecipeCard(Recipe.samples[index]);
+          }),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 0.0,
+      child: Column(
+        children: [
+          // Image.asset(이미지 경로 == recipe.imageUrl)
+          Text(recipe.imageUrl ?? ""),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Text(recipe.label ?? "")
+        ],
+      ),
+    );
+  }
+}
