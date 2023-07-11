9bd2c26e56d808387e011c84f9f07f0724f63399
iyong-il <lyi1233313@gmail.com>
2023-07-11 09:05:24 +0900

    다트 파일 이주

--- tutorials/dart_rps_game/bin/dart_rps_game.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_rps_game/dart_rps_game.dart' as dart_rps_game;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_rps_game.calculate()}!');
-}
--- tutorials/dart_rps_game/test/dart_rps_game_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_rps_game/dart_rps_game.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
