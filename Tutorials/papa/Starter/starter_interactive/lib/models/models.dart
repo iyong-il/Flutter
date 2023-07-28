0c77905c0fd7c0001afc4e325f1266ada8fa6c9c
iyong-il <lyi1233313@gmail.com>
2023-07-28 20:06:33 +0900

    파일삭제

--- Tutorials/papa/Starter/starter_interactive/lib/models/models.dart
+++
@@ -1,7 +0,0 @@
-export 'explore_data.dart';
-export 'explore_recipe.dart';
-export 'post.dart';
-export 'simple_recipe.dart';
-export 'tap_manager.dart';
-export 'grocery_item.dart';
-export 'grocery_manager.dart';
--- Tutorials/papa/Starter/starter_interactive/lib/screens/screens.dart
+++
@@ -1,3 +0,0 @@
-export './explore_screen.dart';
-export './recipes_screen.dart';
-export './to_buy_screen.dart';
